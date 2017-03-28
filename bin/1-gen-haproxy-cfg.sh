b84b435c3d74723378afca03b98a1f23169a639b
zjmeixinyanzhi <zhangjiewanwansui@gmail.com>
2017-03-28 21:33:27 +0800

    20170328213327

--- bin/1-gen-haproxy-cfg.sh
+++ bin/1-gen-haproxy-cfg.sh
@@ -2,8 +2,12 @@
 . ../0-set-config.sh
 target_cfg=../conf/haproxy.cfg
 rm -rf $target_cfg
-cp ../conf/haproxy.cfg.template $target_cfg
-
+cp /etc/haproxy/haproxy.cfg $target_cfg
+if [ $1 = "base" ];then
+  cat ../conf/haproxy/base > $target_cfg 
+else
+  cat ../conf/haproxy/$1 >> $tartget_cfg 
+fi
 sed -i -e 's#bind XX.XX.XX.XX#bind '"$virtual_ip"'#g' $target_cfg
 for ((i=0; i<${#controller_map[@]}; i+=1));
 do
@@ -11,3 +15,4 @@ do
   ip=${controller_map[$name]};
   sed -i -e 's#'"$name"' XX.XX.XX.XX#'"$name"' '"$ip"'#g' $target_cfg
 done;
+
