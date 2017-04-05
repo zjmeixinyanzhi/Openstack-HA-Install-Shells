bc87c5077a7c44f72f59f7936771d385118a02b3
zjmeixinyanzhi <zhangjiewanwansui@gmail.com>
2017-04-05 12:42:07 +0800

    20170405124207

---
+++ bin/add-compute-nodes.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+additionalnodes_name=(${!additionalNodes_map[@]});
+> hosts/additionalNodes.txt
+for ((i=0; i<${#hypervisor_map[@]}; i+=1));
+do
+  name=${hypervisor_name[$i]};
+  ip=${hypervisor_map[$name]};
+  echo "$ip" >> host/additionalNodes.txt
+  echo "$ip" >> hosts/additionalNodes.txt
+done;
+
+base_location=$ftp_info
+deploy_node=compute01
+echo $deploy_node
+blk_name=(${!blks_map[@]});
+
+
+sh_name=compute_nodes_exec.sh
+source_sh=$(echo `pwd`)/sh/$sh_name
+target_sh=/root/tools/t_sh/
+
+### ssh
+for ((i=0; i<${#additionalNodes_map[@]}; i+=1));
+do
+  name=${additionalnodes_name[$i]};
+  ip=${additionalNodes_map[$name]};
+  echo "-------------$name------------"
+  ssh-copy-id root@$ip
+done;
+
+cd /root/my-cluster
+
+osds="";
+echo $osds
+
+### set
+for ((i=0; i<${#additionalNodes_map[@]}; i+=1));
+do
+  name=${additionalnodes_name[$i]};
+  ip=${additionalNodes_map[$name]};
+  echo "-------------$name------------"
+  for ((j=0; j<${#blks_map[@]}; j+=1));
+  do
+    name2=${blk_name[$j]};
+    blk=${blks_map[$name2]};
+    echo "-------------$name2:$blk------------";
+    osds=$osds" "$name":"$blk;
+    ssh root@$ip ceph-disk zap /dev/$blk
+  done
+   scp /etc/ceph/ceph.client.cinder.keyring  root@$name:/etc/ceph/
+   # ssh root@$name  chown -R ceph:ceph $osd_path
+done;
+echo $osds
+### install ceph
+ceph-deploy install --nogpgcheck --repo-url $base_location/download.ceph.com/rpm-$ceph_release/el7/ ${additionalnodes_name[@]} --gpg-url $base_location/download.ceph.com/release.asc
+
+###[部署节点]激活OSD
+ceph-deploy --overwrite-conf osd create $osds
+ceph-deploy admin ${additionalnodes_name[@]}
+
+###查看集群状态
+ceph -s
+### install openstack services
+for ((i=0; i<${#additionalNodes_map[@]}; i+=1));
+do
+  name=${additionalnodes_name[$i]};
+  ip=${additionalNodes_map[$name]};
+  echo "-------------$name------------"
+  ssh root@$ip mkdir -p $target_sh
+  scp $source_sh root@$ip:$target_sh
+  ssh root@$ip chmod -R +x $target_sh
+  ssh root@$ip $target_sh/$sh_name $virtual_ip $local_nic $data_nic $password
+done;
