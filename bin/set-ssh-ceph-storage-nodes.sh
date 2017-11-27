0543ff4fa130a22f33ed0b2855798ddd3648a020
Zhang Jie <zhangjiewanwansui@gmail.com>
2017-11-27 17:41:10 +0800

    Update set-ssh-ceph-storage-nodes.sh

--- bin/set-ssh-ceph-storage-nodes.sh
+++ bin/set-ssh-ceph-storage-nodes.sh
@@ -3,7 +3,6 @@
 nodes_name=(${!nodes_map[@]});
 ### 重新设置/etc/hosts，切换主机名至ceph的存储网段
 cp /etc/hosts /etc/hosts.bak2
-sed -i -e 's#'"$(echo $local_network|cut -d "." -f1-3)"'#'"$(echo $store_network|cut -d "." -f1-3)"'#g' /etc/hosts
 ssh-keygen
 for ((i=0; i<${#nodes_map[@]}; i+=1));
   do
@@ -12,4 +11,5 @@ for ((i=0; i<${#nodes_map[@]}; i+=1));
       echo "-------------$name------------"
       ssh-copy-id root@$ip
       ssh-copy-id root@$name
+      ssh-copy-id root@$(echo $store_network|cut -d "." -f1-3).$(echo $ip|awk -F "." '{print $4}')
   done;
