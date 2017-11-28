1e79d0a81194a519fa9ff197c654141d489a6bcd
Zhang Jie <zhangjiewanwansui@gmail.com>
2017-11-28 10:35:38 +0800

    nova user ssh between compute nodes
    pssh优化，配置计算节点上nova用户的ssh

--- bin/install-configure-compute-nodes-services.sh
+++ bin/install-configure-compute-nodes-services.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 . ../0-set-config.sh
+### 设置计算节点间Nova用户的SSH
+./style/print-split.sh "Set nova user ssh between compute nodes"
+./pssh-exe A "usermod -s /bin/bash nova"
+su - nova -c "ssh-keygen -t rsa"
+cat << EOF > /var/lib/nova/.ssh/config
+Host *
+    StrictHostKeyChecking no
+    UserKnownHostsFile=/dev/null
+EOF
+cat /var/lib/nova/.ssh/id_rsa.pub > /var/lib/nova/.ssh/authorized_keys
+chmod 600 /var/lib/nova/.ssh/authorized_keys
+./scp-exe H /var/lib/nova/.ssh/ /var/lib/nova/
+./pssh-exe H "chown nova:nova -R /var/lib/nova/.ssh/"
+./pssh-exe H "su - nova -c "ssh nova@compute02 cd ~ && pwd""
+### 安装Openstack服务
 ./style/print-split.sh "Openstack Services Installation on Compute Nodes"
 ./scp-exe H compute_nodes_exec.sh /tmp
 ./pssh-exe H "chmod +x /tmp/compute_nodes_exec.sh"
-for ((i=0; i<${#hypervisor_map[@]}; i+=1));
-do
-  name=${nodes_name[$i]};
-  ip=${hypervisor_map[$name]};
-  . style/print-info.sh "$name configuration"  
-  ssh root@$ip /tmp/compute_nodes_exec.sh $virtual_ip $local_nic $data_nic $password
-done;
+./pssh-exe H "/tmp/compute_nodes_exec.sh $virtual_ip $local_nic $data_nic $password"
